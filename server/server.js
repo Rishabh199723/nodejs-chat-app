24d80adeac16112bf78ddacfd16e4f5adecda570
Rishabh <rishabh1997@gmail.com>
2018-08-27 16:17:57 +0530

    broadcast

--- server/server.js
+++ server/server.js
@@ -31,20 +31,43 @@ io.on('connection',(socket)=>       //this is an event listener
   //   createdAt: 123123
   // });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+
   socket.on('createMessage', (message)=> {
     console.log('createMessage', message);
+
+
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
 
   });
+  // socket.broadcast.emit('newMessage', {
+  //     from: message.from,
+  //     text: message.text,
+  //     createdAt: new Date().getTime()
+  //   });
 
 
 
-  socket.on('disconnect', () => {
-    console.log('User Disconnected from server');
-  });
+
+
+});
+socket.on('disconnect', () => {
+  console.log('User Disconnected from server');
 });
 });
 server.listen(port, () => {
